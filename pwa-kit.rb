39f9cef01708cc25b83ba3590f88e61d3e9cba89
Mihai Chindris <chindris-mihai-alexandru@users.noreply.github.com>
2025-11-16 01:04:17 +0200

    Update formula with v0.1.0 release SHA256

--- pwa-kit.rb
+++ pwa-kit.rb
@@ -2,7 +2,7 @@ class PwaKit < Formula
   desc "The macOS toolkit for progressive web apps"
   homepage "https://github.com/chindris-mihai-alexandru/PWA-Kit"
   url "https://github.com/chindris-mihai-alexandru/PWA-Kit/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "PLACEHOLDER_SHA256"  # Will be updated with actual release
+  sha256 "59d73abad9b7b404026726dc96f016de0e2d9c554155c61f7747b55fd0e433ef"
   license "GPL-3.0"
   
   depends_on xcode: ["14.0", :build]
